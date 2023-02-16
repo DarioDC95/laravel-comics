490bbb95d5f8055ad36c1c21f36a2d36f6b8f6b8
DarioDC95 <dario_95@live.com>
2023-02-16 20:19:13 +0100

    Header and Footer Done

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="mycontainer">
+        <div class="myrow">
+            <div class="mycol-md">
+                <img src="{{Vite::asset('resources/img/images/dc-logo.png')}}" alt="Logo Favicon">
+            </div>
+            <div class="mycol-lg">
+                <nav>
+                    <ul>
+                        @foreach ($nav as $item)
+                            <li>
+                                <a class="underline-none" href=" {{ $item['url'] }}">{{ $item['label'] }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nav = config('db.menu');
+    return view('comics', compact('nav'));
 });
